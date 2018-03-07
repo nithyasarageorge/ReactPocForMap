8ad413e9a3b10212d5d3ca559dcf51f318745a1f
nithyasarageorge <nithyasara.george@cognizant.com>
2018-03-07 11:56:18 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import data from "./data";
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+class App extends React.Component {
+  constructor() {
+    super();
+    this.sample = this.sample.bind(this);
+  }
+  sample() {
+    var q = data.map((t) => {
+      var m = t.age * 2;
+      return <div>{m}</div>;
+    });
+    return <div>{q}</div>;
+  }
+  render() {
+      return (
+        <div style={styles}>
+          <Hello name="CodeSandbox" />
+          <h2>Start editing to see some magic happen {'\u2728'}</h2>
+          {this.sample()}
+        </div>
+      );
+    }
+  }
+render(<App />, document.getElementById('root'));
