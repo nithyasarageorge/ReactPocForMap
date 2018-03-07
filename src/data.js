81a8dab0c2d582d5cf66b769bb62473eb3c3b3c4
nithyasarageorge <nithyasara.george@cognizant.com>
2018-03-07 11:56:39 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,19 @@
+var data = [
+  {
+    "name": "abc",
+    "age": 20
+  },
+  {
+    "name": "xyz",
+    "age": 30
+  },
+  {
+    "name": "pqr",
+    "age": 40
+  },
+  {
+    "name": "ghi",
+    "age": 50
+  }
+]
+export default data;
